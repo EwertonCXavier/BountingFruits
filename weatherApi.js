8e6e7eb2c22e51266dbca7e41b0911d6f9a76b26
ewertoncxavier <ewertoncassianox@gmail.com>
2022-12-14 10:33:05 -0400

    openweatherapi integration

--- weatherApi.js
+++ weatherApi.js
@@ -9,27 +9,81 @@ const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${L
 
 const THREE_DAYS_FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${LATITUDE}&lon=${LONGITUDE}&appid=${API_KEY}`;
 
-
 (async () => {
   // current weather api call
   const response = await fetch(WEATHER_API_URL);
   const data = await response.json();
 
+  console.log({ data });
+
+  const currentWeather = {
+    temperature: data.main.temp,
+    humidity: data.main.humidity,
+    description: data.weather[0].description,
+  };
 
   // forecast weather api call
 
   const forecastResponse = await fetch(THREE_DAYS_FORECAST_URL);
-  const forecastData = await forecastResponse.json();
+  const { list } = await forecastResponse.json();
 
+  console.log({ list });
+  const forecastSet = new Set();
 
-  console.log('HERE IS THE API DATA: ', data, forecastData);
-  let currentConditions = {};
-  // if (data) {
-  //   currentConditions = {
-  //     temperature: data.main.temp,
-  //     conditionDescription: data.weather.description,
-  //     humidity: data.main.humidity,
+  // today's date
+  const todayDate = new Date();
+  console.log({
+    todayDate: new Intl.DateTimeFormat("en-US", {
+      dateStyle: "full",
+    }).format(todayDate),
+  });
 
-  //   }
-  // }
+  // Sets today's date
+  const todayDateSpan = document.getElementById("today-date");
+  todayDateSpan.innerText = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+  }).format(todayDate);
+
+  const formattedDate = todayDate.toISOString().match(/\d+\-\d+-\d+/gm)[0];
+  list.forEach((forecast) => {
+    if (!forecast.dt_txt.includes(formattedDate)) {
+      if (forecastSet.size === 0) {
+        forecastSet.add({
+          date: forecast.dt_txt.match(/\d+\-\d+-\d+/gm)[0],
+          temperature: forecast.main.temp,
+        });
+      } else if (
+        forecastSet.size > 0 &&
+        forecastSet.size < 3 &&
+        forecast.dt_txt.match(/\d+\-\d+-\d+/gm)[0] !==
+          [...forecastSet][forecastSet.size - 1].date
+      ) {
+        forecastSet.add({
+          date: forecast.dt_txt.match(/\d+\-\d+-\d+/gm)[0],
+          temperature: forecast.main.temp,
+        });
+      }
+    }
+  });
+
+  console.log("Set: ", forecastSet);
+
+  // Sets the current weather data into the front-end spans
+  const weatherDescriptionSpan = document.getElementById("weather-description");
+  weatherDescriptionSpan.innerHTML = currentWeather.description;
+
+  const currentTemperature = document.getElementById("current-temperature");
+  currentTemperature.innerHTML = currentWeather.temperature;
+
+  const currentHumidity = document.getElementById("current-humidity");
+  currentHumidity.innerHTML = `${currentWeather.humidity}%`;
+
+  [...forecastSet].forEach((forecast, index) => {
+    const forecastDate = document.getElementById(`forecast-day-${index + 1}`);
+    const temperatureForecast = document.getElementById(
+      `forecast-temperature-${index + 1}`
+    );
+    forecastDate.innerHTML = forecast.date;
+    temperatureForecast.innerHTML = forecast.temperature;
+  });
 })();
